7e1bb8c35cbf23adfb1e8ae37c59bbff3189248b
ori-pyro <ori.pyro@gmail.com>
2026-06-24 00:53:53 +0300

    Проход по всем файлам

--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
-#include <string>
-#include <re2/re2.h>
+#include <fstream>      // Чтение файлов
+#include <re2/re2.h>    // Регулярки
+#include <filesystem>
+#include <windows.h>    // Windows API (для нормального вывода кирилицы)
+
+using namespace std;
+namespace fs = filesystem;
+
 
 int main() {
-    // Простейший тест RE2: ищем регулярным выражением ключевые слова модулей
-    std::string test_lexeme = "import-keyword";
-    
-    if (re2::RE2::PartialMatch(test_lexeme, R"(import-keyword|module-keyword|export-keyword)")) {
-        std::cout << "Успех! RE2 нашел лексему: " << test_lexeme << std::endl;
-    } else {
-        std::cout << "Лексема не найдена." << std::endl;
+    // Настраиваем консоль Windows на работу с UTF-8 для обработки кирилицы в пути
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    fs::path dir_path;
+    cout << "Введите путь к директории: ";
+    cin >> dir_path;
+    cout << '\n';
+
+    if (!fs::exists(dir_path)) {
+        cout << "Такой дериктории не существует" << '\n';
+        return 1;
     }
+
+    for (const auto& entry : fs::recursive_directory_iterator(dir_path)) {
+        if (entry.path().extension() == ".cpp") {
+            fstream file(entry.path(), ios::in);        // Создаём объект файла и открываем для чтения
+
+            if (!file) return 1;
+
+            stringstream buffer;
+            buffer << file.rdbuf();
+            string code = buffer.str();                 // Весь код в одну строку
+        }
+    }
+
 
     return 0;
 }
